85fd8da64ccf935b8bcee2be6fb7f0d1a6020f42
vladmany <vladmany99999@gmail.com>
2020-03-27 17:28:50 +0200

    added user roles

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_user = new Role();
+        $role_user->name = 'User';
+        $role_user->save();
+
+        $role_admin = new Role();
+        $role_admin->name = 'Admin';
+        $role_admin->save();
+    }
+}
